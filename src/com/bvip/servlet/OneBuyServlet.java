e8110d767833a4cd5c8a38a70e5704c27f276305
zhht <957399660@qq.com>
2017-01-25 11:03:35 +0800

    201701251103

--- src/com/bvip/servlet/OneBuyServlet.java
+++ src/com/bvip/servlet/OneBuyServlet.java
@@ -1,7 +1,24 @@
 package com.bvip.servlet;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.base.util.BaseServlet;
+import com.base.util.PrintUtils;
+import com.mlm.utils.JDBCUtil;
 
 public class OneBuyServlet extends BaseServlet {
-
+	public void index(HttpServletRequest request,HttpServletResponse response){
+		String sql="select top 20 * from notice order by ptime desc";
+		JDBCUtil ju = new JDBCUtil();
+		String result=null;
+		try{
+			result=ju.QueryToJson(sql, new Object[]{});
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			result=null;
+		}
+		PrintUtils.sendMessageToPhone(response, result);
+	}
 }
